c0a7631f8425c99b0f398a3744b6f603e96e88ba
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2022-12-25 00:28:51 +0600

    vite added

---
+++ src/Redit.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const [searchTerm, setSearchTerm] = React.useState("");
+
+const handleChange = (event) => {
+  console.log(event.target.value);
+};
+
+const Redit = () => {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+
+      <hr />
+      {/* render the list here */}
+      <List />
+    </div>
+  );
+};
+export default Redit;
+
+function List() {
+  return list.map(function (item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+}
