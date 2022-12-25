bc0a617215094b7e80e5aa5460265c180cd4619f
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2022-12-25 22:02:59 +0600

    useContext added

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,19 +1,19 @@
-import { useState, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import ErrorBoundary from "./ErrorBoundary";
+import { useContext, useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import Modal from "./Modal";
 
 const Details = () => {
+  const { id } = useParams();
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  const [, setAdoptedPet] = useState(AdoptedPetContext.getCurrent);
-
-  const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
   if (results.isLoading) {
     return (
@@ -22,38 +22,35 @@ const Details = () => {
       </div>
     );
   }
+
   const pet = results.data.pets[0];
 
   return (
     <div className="details">
-      <Carousel images={pet.images} />;
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
-        <h2>
-          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
-          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>;
-          <p>{pet.description}</p>
-          {
-            showModal ? (
-              <Modal>
-                <div>
-                  <h1>Would you like to adopt {pet.name}?</h1>
-                  <div className="buttons">
-                    <button
-                      onClick={() => {
-                        setAdoptedPet(pet);
-                        navigate("/");
-                      }}
-                    >
-                      Yes
-                    </button>
-                    <button onClick={() => setShowModal(false)}>No</button>
-                  </div>
-                </div>
-              </Modal>
-            ) : null //; you have to remove this semi-colon, my auto-formatter adds it back if I delete it
-          }
-        </h2>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useContext, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
 import AdoptedPetContext from "./AdoptedPetContext";
@@ -7,12 +7,12 @@ import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [adoptedPet] = useContext(AdoptedPetContext);
   const [requestParams, setRequestParams] = useState({
     location: "",
     animal: "",
     breed: "",
   });
+  const [adoptedPet] = useContext(AdoptedPetContext);
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
 
@@ -33,14 +33,11 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
-        {
-          adoptedPet ? (
-            <div className="pet image-container">
-              <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
-            </div>
-          ) : null //; you have to remove this semi-colon, my auto-formatter adds it back if I delete it
-        }
-
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
           <input id="location" name="location" placeholder="Location" />
