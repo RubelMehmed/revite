52532d72aaa3309ae56f5c3e117cc614d0ce9030
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-10-27 16:38:01 +0600

    feat: extended tailwind

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -22,7 +22,7 @@ const SearchParams = () => {
   return (
     <div className="my-0 mx-auto w-11/12">
       <form
-        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col items-center justify-center"
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -41,12 +41,13 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input 
+          <input
             type="text"
             id="location"
             name="location"
-            className="mb-5 block w-60"
-            placeholder="Location" />
+            placeholder="Location"
+            className="search-input"
+          />
         </label>
 
         <label htmlFor="animal">
@@ -54,13 +55,13 @@ const SearchParams = () => {
           <select
             id="animal"
             name="animal"
-            className="mb-5 block w-60"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
             onBlur={(e) => {
               setAnimal(e.target.value);
             }}
+            className="search-input"
           >
             <option />
             {ANIMALS.map((animal) => (
@@ -73,24 +74,24 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select 
-            id="breed" name="breed"
-            className="mb-5 block w-60 disabled: opacity-50"
+          <select
             disabled={!breeds.length}
-            >
+            id="breed"
+            name="breed"
+            className="search-input grayed-out-disabled"
+          >
             <option />
             {breeds.map((breed) => (
-              <option 
-                key={breed}
-                value={breed}
-              >
+              <option key={breed} value={breed}>
                 {breed}
               </option>
             ))}
           </select>
         </label>
 
-        <button className="rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50">Submit</button>
+        <button className="rounded px-6 py-2 color text-white hover:opacity-50 border-none bg-orange-500">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
@@ -98,3 +99,106 @@ const SearchParams = () => {
 };
 
 export default SearchParams;
+
+//________________________________________
+
+// import { useQuery } from "@tanstack/react-query";
+// import { useContext, useState } from "react";
+// import AdoptedPetContext from "./AdoptedPetContext";
+// import fetchSearch from "./fetchSearch";
+// import Results from "./Results";
+// import useBreedList from "./useBreedList";
+// const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+// const SearchParams = () => {
+//   const [requestParams, setRequestParams] = useState({
+//     location: "",
+//     animal: "",
+//     breed: "",
+//   });
+//   const [adoptedPet] = useContext(AdoptedPetContext);
+//   const [animal, setAnimal] = useState("");
+//   const [breeds] = useBreedList(animal);
+
+//   const results = useQuery(["search", requestParams], fetchSearch);
+//   const pets = results?.data?.pets ?? [];
+
+//   return (
+//     <div className="my-0 mx-auto w-11/12">
+//       <form
+//         className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col items-center justify-center"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           const formData = new FormData(e.target);
+//           const obj = {
+//             animal: formData.get("animal") ?? "",
+//             breed: formData.get("breed") ?? "",
+//             location: formData.get("location") ?? "",
+//           };
+//           setRequestParams(obj);
+//         }}
+//       >
+//         {adoptedPet ? (
+//           <div className="pet image-container">
+//             <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+//           </div>
+//         ) : null}
+//         <label htmlFor="location">
+//           Location
+//           <input 
+//             type="text"
+//             id="location"
+//             name="location"
+//             className="mb-5 block w-60"
+//             placeholder="Location" />
+//         </label>
+
+//         <label htmlFor="animal">
+//           Animal
+//           <select
+//             id="animal"
+//             name="animal"
+//             className="mb-5 block w-60"
+//             onChange={(e) => {
+//               setAnimal(e.target.value);
+//             }}
+//             onBlur={(e) => {
+//               setAnimal(e.target.value);
+//             }}
+//           >
+//             <option />
+//             {ANIMALS.map((animal) => (
+//               <option key={animal} value={animal}>
+//                 {animal}
+//               </option>
+//             ))}
+//           </select>
+//         </label>
+
+//         <label htmlFor="breed">
+//           Breed
+//           <select 
+//             id="breed" name="breed"
+//             className="mb-5 block w-60 disabled: opacity-50"
+//             disabled={!breeds.length}
+//             >
+//             <option />
+//             {breeds.map((breed) => (
+//               <option 
+//                 key={breed}
+//                 value={breed}
+//               >
+//                 {breed}
+//               </option>
+//             ))}
+//           </select>
+//         </label>
+
+//         <button className="rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50">Submit</button>
+//       </form>
+//       <Results pets={pets} />
+//     </div>
+//   );
+// };
+
+// export default SearchParams;
